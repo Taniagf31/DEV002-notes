d46521818e1bfb8e5d0daa267f46c9380710eacd
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-01-23 19:56:18 -0600

    cambio de rama

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom";
 import './index.css';
 
-const app = <h1>Desarrollo Útil</h1>;
+const app = <h1>Journal Note</h1>;
 const container = document.getElementById("root");
+
 
 ReactDOM.render(app, container);
