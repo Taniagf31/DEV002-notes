4e768a20eeeb671233e7cb20e6e598e0738fdd58
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-01-25 11:42:41 -0600

    mostrar primer componente en pantalla

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,36 @@
-import ReactDOM from "react-dom";
-// import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import './index.css';
+import JournalNoteApp from "./JournalNoteApp";
 
-const LoginBox = ({ title, username, password, login }) => {
-    return (
-        <div>
-            <h2>{title}</h2>
-            <div>
-                <span>{username}</span>
-                <span>{password}</span>
-            </div>
-            <button>{login}</button>
-        </div>
-    );
-};
-
-const JournalNoteApp = (
-    <div>
-        <h1>Landing Page Journal Note</h1>
-        <LoginBox
-            title='Login'
-            username='username'
-            password='password'
-            login= 'login'
-        ></LoginBox>
-    </div>);
-
-const container = document.getElementById("root");
-ReactDOM.render(JournalNoteApp, container);
+const root = ReactDOM.createRoot (document.getElementById("root"));
+// root.render(<h1>♥ Journal Note ♥</h1>);
+root.render(
+<JournalNoteApp></JournalNoteApp>
+);
 
 
 
 
 
-// const container = document.getElementById("root");
-// const root = createRoot(container);
-// root.render(<JournalNoteApp />);
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
-// reportWebVitals();
+
+
+
+
+
+
+
+
+// const JournalNoteApp = (
+//     <div>
+//     <h1>Journal Note</h1>
+//     <LoginBox 
+//     title = "Login"
+//     username = ""
+//     password= ""
+//     login = "Login"
+//     ></LoginBox>
+//     </div>);
+    
