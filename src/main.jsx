cf60c57e4e6572b07bf5ddba722f1fa2c58f9c26
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-01-26 16:28:49 -0600

    instalación y configuración de Eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,20 @@
-import ReactDOM from "react-dom/client";
-import './index.css';
-import JournalNoteApp from "./JournalNoteApp";
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import JournalNoteApp from './JournalNoteApp'
 
-const root = ReactDOM.createRoot (document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(<h1>♥ Journal Note ♥</h1>);
 root.render(
 <JournalNoteApp></JournalNoteApp>
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+)
 
 // const JournalNoteApp = (
 //     <div>
 //     <h1>Journal Note</h1>
-//     <LoginBox 
+//     <LoginBox
 //     title = "Login"
 //     username = ""
 //     password= ""
 //     login = "Login"
 //     ></LoginBox>
 //     </div>);
-    
