664599553ae467a81592e992da5134417f14a7e2
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-01-27 19:37:25 -0600

    cambios de css y primer componente longinbox

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,8 @@ import JournalNoteApp from './JournalNoteApp'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(<h1>♥ Journal Note ♥</h1>);
 root.render(
-<JournalNoteApp></JournalNoteApp>
+<JournalNoteApp />
+
 )
 
-// const JournalNoteApp = (
-//     <div>
-//     <h1>Journal Note</h1>
-//     <LoginBox
-//     title = "Login"
-//     username = ""
-//     password= ""
-//     login = "Login"
-//     ></LoginBox>
-//     </div>);
+
