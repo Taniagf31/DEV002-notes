1ee7d3d373d925e2c8d587bc7f22f8466745b60f
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-01-31 13:38:10 -0600

    agregando authcontext

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
+import { AuthProvider } from './context/authContext';
 
 function App() {
     return (
-        <Routes>
+        <div className='page'>
+            
+      <AuthProvider>
+      <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-
         </Routes>
+
+      </AuthProvider>
+        </div>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,11 @@
+import { useContext } from 'react';
+import {context} from '../context/authContext'
 
- export function Home (){
+export function Home (){
+
+const authContext = useContext(context)
+
+
     return <h1>💗Home💗</h1>;
 }
 
