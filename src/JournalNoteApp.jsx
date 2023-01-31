6b113cfea1fd75235f647b2cdf36e69bf18b5099
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-01-30 21:37:13 -0600

    primer intento de enrutado

--- src/JournalNoteApp.jsx
+++ src/JournalNoteApp.jsx
@@ -1,13 +1,21 @@
+
+import {Routes, Route} from 'react-router-dom'
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
 import "./components/JournalNoteApp.css"
-import LoginBox from "./components/LoginBox";
+
+// import LoginBox from "./components/LoginBox";
 
 function JournalNoteApp() {
     return (
-        <div className="JournalNote_logo">
-            <img src="./Resource/Logo-JN.png" alt="logo" />
-            <LoginBox />
-
-        </div>
+        <Routes>
+            <Route path= "/" element={<Home/>} />
+            <Route path= "/" element={<Login/>} />
+            <Route path= "/" element={<Register/>} />
+            
+            </Routes>
     )
 }
+
 export default JournalNoteApp;
