689b89a951a70f081bc49f72932af85e54f697ca
Cony GuGa <constanza.garcia.gtz@gmail.com>
2023-02-05 12:53:54 -0600

    Firestore - Pruebas Iniciales2

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"; //Esto no estaba inactivo...
 import {getAuth} from 'firebase/auth';
 import './firebase';
 
