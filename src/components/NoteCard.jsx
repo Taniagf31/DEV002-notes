dc48b4ce86c61e52164f7815db13fcb838d6f0a4
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-02-08 00:35:57 -0600

    cambios en note-card CSS y login

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,18 @@
+import "./css-components/NoteCard.css";
 export function NoteCard({ task, deleteNote }) {
 
     return (
-        <div >
-            <h1>{task.title} </h1>
-            <p>{task.description} </p>
-            <button onClick={() => deleteNote (task.id)}>
-                Delete Note
-            </button>
-        </div>
+        <>
+            <div className="container-all">
+            <div className="container-note">
+            <h2 className="card-title">{task.title} </h2>
+            <p className="nc-description">{task.description} </p>
+            <div className="dad-delete">
+            <button onClick={() => deleteNote (task.id)} className= "btn-delete">
+                Delete
+            </button></div>
+            </div>
+            </div>
+        </>
     );
 }
