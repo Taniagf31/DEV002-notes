5dbc7f942ae3883ad2d7fdff3b2ca752f0972df7
tania lizbeth garcia fierro <taniafe98@gmail.com>
2023-02-08 21:31:43 -0600

    cambios en CSS notas

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
             element={
               <ProtectedRoute>
                 <Home />
-
               </ProtectedRoute>
               
             } />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import { useAuth } from "../context/authContext";
-import {TaskList} from './TaskList';
-import {TaskForm} from './TaskForm';
-import {tasks as data} from './tasks';
-import {useState, useEffect} from 'react';
+import { TaskList } from './TaskList';
+import { TaskForm } from './TaskForm';
+import { tasks as data } from './tasks';
+import { useState, useEffect } from 'react';
 import "./css-components/home.css";
+
+// import { db } from '../Firebase';
+
 
 export function Home() {
     const { user, logout, loading } = useAuth()
@@ -12,44 +15,42 @@ export function Home() {
         setTasks(data)
     }, []
     )
-   function createNote(task) {
-    setTasks([...tasks, {
-     title: task.title, 
-     id: tasks.length,
-     description: task.description  
-    } ])
-   }
-   
-   function deleteNote (taskId) {
-    setTasks (tasks.filter (task => task.id !== taskId))
-   }
+    function createNote(task) {
+        setTasks([...tasks, {
+            title: task.title,
+            id: tasks.length,
+            description: task.description
+        }])
+    }
+
+    function deleteNote(taskId) {
+        setTasks(tasks.filter(task => task.id !== taskId))
+    }
 
     const handledLogout = async () => {
         try {
-        await logout();
-        } catch(error){
+            await logout();
+        } catch (error) {
             console.error(error);
         }
     };
     if (loading) return <h2>Loading</h2>
     return <div>
         <div className="dad-logout">
-        <button onClick={handledLogout} className="btn-logout" >Logout</button>
+            <button onClick={handledLogout} className="btn-logout" >Logout</button>
         </div>
 
         <h1 className="title-page">💗✨Welcome to Journal Note✨💗
-        <br />
-         {user.displayName||user.email}</h1>
+            <br />
+            {user.displayName || user.email}</h1>
         <h2>Remember this is important to you !</h2>
-        
-       
-           <TaskForm createNote = {createNote} />
-           <div className="container-notes">
-            <TaskList tasks = {tasks} deleteNote={deleteNote} />
 
 
-       </div>
-    </div>  
+        <TaskForm createNote={createNote} />
+        <div className="container-notes">
+            <TaskList tasks={tasks} deleteNote={deleteNote} />
+        </div>
+    </div>
 }
 
 
